6914740272007b537a1eb1f1068b143b231d915b
cominoapp <cs@comino.app>
2025-10-25 00:19:30 +0900

    Fix api-client.ts to use production URL

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // API 베이스 URL
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://otonaba-backend.onrender.com/api';
 
 // axios 인스턴스 생성
 const apiClient = axios.create({
